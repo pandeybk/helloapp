7744b3dcac85d69509594e3e0b9535c85bebfab2
Balkrishna Pandey <sachit.nep@gmail.com>
2023-01-09 13:40:56 -0700

    run directly
    Signed-off-by: Balkrishna Pandey <sachit.nep@gmail.com>

--- s2i-app/app.py
+++ s2i-app/app.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, Source To Image"
+
+if __name__ == '__main__': 
+    app.run(host="0.0.0.0", port=8000, debug=True,use_reloader=True)
