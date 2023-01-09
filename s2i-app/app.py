c3678ce15a107d533b3d723b9549e2d94cb7ce97
Balkrishna Pandey <sachit.nep@gmail.com>
2023-01-09 13:48:36 -0700

    port change to 8080
    Signed-off-by: Balkrishna Pandey <sachit.nep@gmail.com>

--- s2i-app/app.py
+++ s2i-app/app.py
@@ -5,4 +5,4 @@ def index():
     return "Hello, Source To Image"
 
 if __name__ == '__main__': 
-    app.run(host="0.0.0.0", port=8000, debug=True,use_reloader=True)
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
